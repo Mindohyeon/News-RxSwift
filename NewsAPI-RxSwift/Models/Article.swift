fdc2fe8effbd8e07e41243b9135b435b3fa1513c
Mindohyeon <min50875@gmail.com>
2022-05-05 23:07:48 +0900

    test

---
+++ NewsAPI-RxSwift/Models/Article.swift
@@ -0,0 +1,23 @@
+//
+//  Article.swift
+//  NewsAPI-RxSwift
+//
+//  Created by 민도현 on 2022/05/05.
+//
+
+import Foundation
+
+struct ArticleResponse {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+}
---
+++ NewsAPI-RxSwift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewsAPI-RxSwift
+//
+//  Created by 민도현 on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
