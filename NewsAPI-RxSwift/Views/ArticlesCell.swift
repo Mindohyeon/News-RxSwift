445230e6c4f3b3e0d3bbfdf205756acefe87ca9e
Mindohyeon <min50875@gmail.com>
2022-05-07 21:58:33 +0900

    SnapKit 쓰지 않고 UI 배치하기

---
+++ NewsAPI-RxSwift/Views/ArticlesCell.swift
@@ -0,0 +1,69 @@
+//
+//  ArticlesCell.swift
+//  NewsAPI-RxSwift
+//
+//  Created by 민도현 on 2022/05/07.
+//
+import UIKit
+import RxSwift
+
+class ArticlesCell: UICollectionViewCell {
+    var viewModel = PublishSubject<ArticleViewModel>()
+    
+    lazy var imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.layer.cornerRadius = 8
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        iv.backgroundColor = .secondarySystemBackground
+        
+        return iv
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 4
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        backgroundColor = .systemBackground
+        
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: imageView.rightAnchor, constant: 40).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -30).isActive = true
+        
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: titleLabel.leftAnchor).isActive = true
+        descriptionLabel.rightAnchor.constraint(equalTo: titleLabel.rightAnchor).isActive = true
+    }
+    
+}
